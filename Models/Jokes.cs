ab8e0eb102d4ff8b5a1d28ddaa568f45032f1a09
manou141 <manou141@hotmail.com>
2022-11-26 00:45:03 -0500

    first

---
+++ Models/Jokes.cs
@@ -0,0 +1,13 @@
+﻿namespace JokesApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public String JokeQuestion  { get; set; }
+        public String JokeAnswer  { get; set; }
+        public Jokes()
+        {
+
+        }
+    }
+}
